9be5fff2085bfc176a112018941f1cedea3ac092
wubo1990 <bowu@ucdavis.edu>
2014-10-17 13:47:27 -0700

    Add galileo project

--- galileo/ViewController.h
+++ galileo/ViewController.h
@@ -8,6 +8,20 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface ViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIButton *panClockwiseButton;
+@property (weak, nonatomic) IBOutlet UIButton *panAnticlockwiseButton;
+@property (weak, nonatomic) IBOutlet UIButton *tiltClockwiseButton;
+@property (weak, nonatomic) IBOutlet UIButton *tiltAnticlockwiseButton;
+@property (weak, nonatomic) IBOutlet UILabel *statusLabel;
+
+
+-(IBAction)panClockwise:(id)sender;
+-(IBAction)panAnticlockwise:(id)sender;
+-(IBAction)tiltClockwise:(id)sender;
+-(IBAction)tiltAnticlockwise:(id)sender;
+
 
 @end
--- galileo/ViewController.m
+++ galileo/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <GalileoControl/GalileoControl.h>
 
-@interface ViewController ()
+@interface ViewController () <GCGalileoDelegate>
 
 @end
 
@@ -18,6 +19,92 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    [GCGalileo sharedGalileo].delegate = self;
+    [[GCGalileo sharedGalileo] waitForConnection];
+}
+
+
+- (void) galileoDidConnect
+{
+    UIAlertView* galileoConnectedAlert = [[UIAlertView alloc] initWithTitle:@"Galileo connected!"
+                                                                        message:nil
+                                                                       delegate:nil
+                                                              cancelButtonTitle:@"OK"
+                                                              otherButtonTitles:nil];
+    [galileoConnectedAlert show];
+}
+
+/*
+- (void) galileoDidConnect
+{
+    GCPositionControl* panPositionControl = [[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisPan];
+    [panPositionControl incrementTargetPosition:90.0 notifyDelegate:nil waitUntilStationary:NO];
+}
+*/
+- (void) galileoDidDisconnect
+{
+    [[GCGalileo sharedGalileo] waitForConnection];
+}
+
+
+-(IBAction)panClockwise:(id)sender{
+    /*
+    void (^completionBlock) (BOOL) = ^(BOOL wasCommandPreempted)
+    {
+        if (!wasCommandPreempted) [self controlDidReachTargetPosition];
+    };
+    [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisPan] incrementTargetPosition:360
+                                                                                 completionBlock:completionBlock waitUntilStationary:NO];
+     */
+    
+    GCPositionControl* panPositionControl = [[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisPan];
+    [panPositionControl incrementTargetPosition:360.0 notifyDelegate:nil waitUntilStationary:NO];
+}
+
+
+-(IBAction)panAnticlockwise:(id)sender{
+    /*
+    void (^completionBlock) (BOOL) = ^(BOOL wasCommandPreempted)
+    {
+        if (!wasCommandPreempted) [self controlDidReachTargetPosition];
+    };
+    [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisPan] incrementTargetPosition:-360
+                                                                                 completionBlock:completionBlock
+                                                                             waitUntilStationary:NO];
+    */
+    
+    //GCVelocityControl* tiltVelocityControl = [[GCGalileo sharedGalileo] velocityControlForAxis:GCControlAxisTilt];
+    //[tiltVelocityControl setTargetVelocity:200.0];
+    GCPositionControl* panPositionControl = [[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisPan];
+    [panPositionControl incrementTargetPosition:-360.0 notifyDelegate:nil waitUntilStationary:NO];
+}
+
+
+-(IBAction)tiltClockwise:(id)sender{
+    void (^completionBlock) (BOOL) = ^(BOOL wasCommandPreempted)
+    {
+        if (!wasCommandPreempted) [self controlDidReachTargetPosition];
+    };
+    [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisTilt] incrementTargetPosition:360
+                                                                                  completionBlock:completionBlock
+                                                                              waitUntilStationary:NO];
+}
+
+
+-(IBAction)tiltAnticlockwise:(id)sender{
+    void (^completionBlock) (BOOL) = ^(BOOL wasCommandPreempted)
+    {
+        if (!wasCommandPreempted) [self controlDidReachTargetPosition];
+    };
+    [[[GCGalileo sharedGalileo] positionControlForAxis:GCControlAxisTilt] incrementTargetPosition:-360
+                                                                                  completionBlock:completionBlock
+                                                                              waitUntilStationary:NO];
+}
+
+- (void) controlDidReachTargetPosition
+{
+    // Re-enable the UI now that the target has been reached, assuming we are still connected to Galileo
+    if ([[GCGalileo sharedGalileo] isConnected]);
 }
 
 - (void)didReceiveMemoryWarning
